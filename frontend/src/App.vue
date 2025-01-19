d394d19d60a1c3b5a3acf0cf58126c9153102fbc
Everaldo Junior <jr.o2@hotmail.com>
2025-01-19 09:47:28 -0300

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <h1>My Blog</h1>
+    <!-- Add your own components here -->
+  </div>
+</template>
+
+<style>
+/* Add your own CSS styles here */
+</style>
+
+<script>
+export default {
+  // Add your own JavaScript code here
+}
+</script>
